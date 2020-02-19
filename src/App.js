e552e87566a26d84e94a966394d54856364ab357
draganasavanovic <dragana.savanovic@wayseven.com>
2020-02-19 13:56:48 +0100

    useEffect after render

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import HookCounter from "./components/HookCounter";
 import HookCounterTwo from "./components/HookCounterTwo";
 import HookCounterThree from "./components/HookCounterThree";
 import HookCounterFour from "./components/HookCounterFour";
+import HookCounterOne from "./components/HookCounterOne";
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour />
+      <HookCounterOne />
+      {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterTwo /> */}
       {/* <ClassCounter /> */}
